c0ea109bd1827f0421c52743af9003c778bc1a14
MEDazizlad <aladhari@effios-expertise.com>
2023-07-21 20:39:04 +0100

    Added new imported SideBar: 	modified:   src/App.js 	renamed:    src/components/SideBar/axia.png -> src/components/DepricatedSideBar/axia.png 	renamed:    src/components/SideBar/index.js -> src/components/DepricatedSideBar/index.js 	new file:   src/components/ImportedSideBar/HorizontalLayout/index.js 	new file:   src/components/ImportedSideBar/LayoutMenuData.js 	new file:   src/components/ImportedSideBar/TwoColumnLayout/index.js 	new file:   src/components/ImportedSideBar/VerticalLayouts/index.js 	new file:   src/components/ImportedSideBar/axia.png 	new file:   src/components/ImportedSideBar/index.js 	new file:   src/components/common/withRouter.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
 import { React, useState } from "react";
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SideBar from "./components/SideBar";
+import SideBar from "./components/ImportedSideBar";
 import ClientList from "./components/Client";
 import InterventionList from "./components/Intervention";
 import ComplaintList from "./components/Complaint";
 import ProjectList from "./components/Project";
-import './App.css';
+import "./App.css";
 
 const App = () => {
-    const [sidebarIsOpen, setSidebarOpen] = useState(true);
-    const [activeLink, setActiveLink] = useState("clients"); // Set "clients" as the initial active link
+  const [sidebarIsOpen, setSidebarOpen] = useState(true);
+  const [activeLink, setActiveLink] = useState("clients"); // Set "clients" as the initial active link
 
-    const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
+  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
 
-    const handleLinkClick = (link) => {
-        setActiveLink(link);
-    };
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+  let contentComponent;
 
-    let contentComponent;
+  // Render the corresponding component based on the active link
+  if (activeLink === "clients") {
+    contentComponent = <ClientList />;
+  } else if (activeLink === "interventions") {
+    contentComponent = <InterventionList />;
+  } else if (activeLink === "complaints") {
+    contentComponent = <ComplaintList />;
+  } else if (activeLink === "projects") {
+    contentComponent = <ProjectList />;
+  }
 
-    // Render the corresponding component based on the active link
-    if (activeLink === "clients") {
-        contentComponent = <ClientList />;
-    } else if (activeLink === "interventions") {
-        contentComponent = <InterventionList />;
-    } else if (activeLink === "complaints") {
-        contentComponent = <ComplaintList/>;
-    } else if (activeLink  === "projects"){
-        contentComponent = <ProjectList/>;
-    }
-
-    return (
-        <Router>
-            <div className="App wrapper">
-                <SideBar
-                    toggle={toggleSidebar}
-                    isOpen={sidebarIsOpen}
-                    activeLink={activeLink}
-                    handleLinkClick={handleLinkClick}
-                />
-                {contentComponent}
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App wrapper">
+        <SideBar
+          toggle={toggleSidebar}
+          isOpen={sidebarIsOpen}
+          activeLink={activeLink}
+          handleLinkClick={handleLinkClick}
+        />{" "}
+        {contentComponent}
+      </div>
+    </Router>
+  );
 };
 
 export default App;
